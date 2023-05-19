4c005acef612ee371584f49ab9fa86c46b7906de
Kirill Khramkov <hramkir@gmail.com>
2023-05-19 05:05:52 +0400

    Stage 2 done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +7,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write(b"+PONG\r\n").expect("write failed");
+                stream.flush().expect("flush failed");
             }
             Err(e) => {
                 println!("error: {}", e);
